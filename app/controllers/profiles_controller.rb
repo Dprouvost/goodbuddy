388ec67bc722bee2072778a15bbe9ae05a783ba6
Dimitri Prouvost <dimitriprouvost@hotmail.com>
2019-03-08 14:39:08 +0100

    matching

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,115 +1,128 @@
 class ProfilesController < ApplicationController
 
+  def commun_goals_profile_sample
+    current_user.profile.categories.map do |category|  # definir le current_user
+      category.profiles.ids.where.not(id: current_user.profile.id)
+    end.uniq
+  end
+
   def total_score
   # Objectif: definir un total_score
-    # pour chacun des profiles selectionnés (ceux qui ont des commun_goals)
-    # en les pondérant (en fonction du choix du current user)
-    # score_goal x goal_weight, score_social x social_weight, score_language x language_weight, score_style x style_weight, score_experience x experience_weight
-  end
-
-  def commun_goals_profile_sample
-    profile_sample = []
-    current_user.profile.categories.each do |cat|
-      profile_sample << cat.profiles.where.not(id: current_user.profile.id)
+    commun_goals_profile_sample.map do |id|
+      goal_weight = 0.5 * (social_weight + language_weight + style_weight + experience_weight)
+      score = ((score_goal(id) * goal_weight) + (score_social(id) * social_weight) + (score_language(id) * language_weight) + (score_style(id) * style_weight) + (score_experience(id) * experience_weight)) / (goal_weight + social_weight + language_weight + style_weight + experience_weight)
+      [id, score]
     end
-    profile_sample.flatten.uniq
   end
 
-  def score_goal
+  def score_goal(id)
   # Objectif: definir un score_goal en fonction du nombre de goals en commun
-    # 1- Pour chaque profile selectionné, calculer un score_goal, comme ci dessous:
-      # score_goal = (nombre de goal.label en communs avec le current_profile / nombre total de goal.label du current_profile) x 100
-
-    current_goals = []
-    current_goals << current_user.profile.categories.where(stamp: 'goal')
-    commun_goals_profile_sample.each do |profile|
-      ((profile.categories.include?(current_goals).count)/(current_goals.count))/100
-    end
+    (((Profile.find(id).categories.where(stamp: 'goal') + current_user.profile.categories.where(stamp: 'goal')).uniq.count)/((current_user.profile.categories.where(stamp: 'goal')).count))*100
   end
 
-  def score_social
+  def score_social(id)
   # Objectif: definir un score_social en fonction du nombre de socials en commun
-    # 1- Pour chaque profile, calculer un score_social, comme ci dessous:
-      # score_social = (nombre de social.label en communs avec le current_profile / nombre total de social.label) x 100
-
-    current_socials = []
-    current_socials << current_user.profile.categories.where(stamp: 'social')
-    commun_goals_profile_sample.each do |profile|
-      ((profile.categories.include?(current_socials).count)/(socials.count))/100
-    end
+    (((Profile.find(id).categories.where(stamp: 'social') + current_user.profile.categories.where(stamp: 'social')).uniq.count)/((current_user.profile.categories.where(stamp: 'social')).count))*100
   end
 
-  def score_language
-
-
+  def score_language(id)
   # Objectif: definir un score_language en fonction du nombre de language en commun mais aussi de leur ordre
-  currentUserLanguages = ["L1","L2","L3"]
-  otherUsersLanguages = [["L1","L2","L3"],["L2","L1","L3"],["L4","L2","L1"],["L3","L2","L1"]]
 
-    # 1- Créer le hash/array_language du current_profile avec ses 3 languages favoris via l'API de github, 3 clés : language_1, language_2 et language_3
+    current_user_language_array = []
+    current_user_language_array << current_user.profile.tecnhical.language_1
+    current_user_language_array << current_user.profile.tecnhical.language_2
+    current_user_language_array << current_user.profile.tecnhical.language_3
+    
+    profile_user_language_array = []
+    profile_user_language_array << Profile.find(id).technical.language_1
+    profile_user_language_array << Profile.find(id).technical.language_2
+    profile_user_language_array << Profile.find(id).technical.language_3
 
-    # 2- Sortir le même hash/array_language de tous les profiles de la DB qui ont au moins un commun_goal
+    if current_user_language_array[0] = profile_user_language_array[0]
+      scoreL1 = 100
+    elsif current_user_language_array[0] = profile_user_language_array[1]
+      scoreL1 = 90
+    elsif current_user_language_array[0] = profile_user_language_array[2]
+      scoreL1 = 80
+    end
 
-    # 3- Pour chaque profile selectionné, comparer les valeurs de leur hash/array celles du current_profile
+    if current_user_language_array[1] = profile_user_language_array[0]
+      scoreL2 = 70
+    elsif current_user_language_array[1] = profile_user_language_array[1]
+      scoreL2 = 90
+    elsif current_user_language_array[1] = profile_user_language_array[2]
+      scoreL2 = 50
+    end
 
-    # 4- Pour chaque profile, ajouter un score_language calculé comme ci dessous:
-      # score_l1
-      # if l1 = l1, score_l1 = 1.0
-      # if l1 = l2, score_l1 = 0.7
-      # if l1 = l3, score_l1 = 0.6
-      # else score_l1 = 0
-    # score_l2
-      # if l2 = l1, score_l2 = 0.7
-      # if l2 = l2, score_l2 = 0.9
-      # if l2 = l3, score_l2 = 0.5
-      # else score_l2 = 0
-    # score_l3
-      # if l3 = l1, score_l3 = 0.6
-      # if l3 = l2, score_l3 = 0.5
-      # if l3 = l3, score_l3 = 0.8
-      # else score_l3 = 0
-    # score_language = ((score_l1 + score_l2 + score_l3) / max_score) x 100
+    if current_user_language_array[2] = profile_user_language_array[0]
+      scoreL3 = 60
+    elsif current_user_language_array[2] = profile_user_language_array[1]
+      scoreL3 = 50
+    elsif current_user_language_array[2] = profile_user_language_array[2]
+      scoreL3 = 80
+    end
+
+    (scoreL1 + scoreL2 + scoreL3) / 270 # à variabiliser pour autoriser le changement de score
 
   end
 
-  def score_style
+  def score_style(id)
   # Objectif: definir un score_style en fonction du commit_slot
-    # 1- Pour chaque profile, calculer un score_style, comme ci dessous:
-      # score_style =
-        # if current_profile.commit_slot = profile.commit_slot, score_style = 100
-        # if (current_profil.commit_slot - profile.commit_slot).abs = 1, score_style = 75
-        # if (current_profil.commit_slot - profile.commit_slot).abs = 2, score_style = 50
-        # if (current_profil.commit_slot - profile.commit_slot).abs = 3, score_style = 0
-
-    current_style = current_user.profile.technicals.commit_slot
-    profile_sample.each do |profile|
-      if current_user.profile.technicals.commit_slot = profile.technicals.commit_slot
-        score_style = 100
-      elsif (current_user.profile.technicals.commit_slot = profile.technicals.commit_slot).abs = 1
-        score_style = 75
-      elsif (current_user.profile.technicals.commit_slot = profile.technicals.commit_slot).abs = 2
-        score_style = 50
-      else
-        score_style = 0
-      end
+    case (Profile.find(id).technical.commit_slot - current_user.profile.technical.commit_slot).abs 
+    when 0
+      100
+    when 1 
+      75
+    when 2 
+      50
+    when 3
+      0
     end
+  end
+
+  def github_age_score(id)
+    array = []
+    difference = (Profile.find(id).technical.github_age - current_user.profile.technical.github_age).abs
+    commun_goals_profile_sample.each do |element|
+      array << Profile.find(element).technical.github_age # ajouter celui du current user
+    end
+    max_score = array.max
+    (1-(difference/max_score)) * 100
+  end
+
+  def number_of_projects_score(id)
+    array = []
+    difference = (Profile.find(id).technical.number_of_projects - current_user.profile.technical.number_of_projects).abs
+    commun_goals_profile_sample.each do |element|
+      array << Profile.find(element).technical.number_of_projects # ajouter celui du current user
+    end
+    max_score = array.max
+    (1-(difference/max_score)) * 100
+  end
+
+  def total_commits_score(id)
+    array = []
+    difference = (Profile.find(id).technical.total_commits - current_user.profile.technical.total_commits).abs
+    commun_goals_profile_sample.each do |element|
+      array << Profile.find(element).technical.total_commits # ajouter celui du current user
+    end
+    max_score = array.max
+    (1-(difference/max_score)) * 100
+  end
+
+  def followers_score(id)
+    array = []
+    difference = (Profile.find(id).technical.followers - current_user.profile.technical.followers).abs
+    commun_goals_profile_sample.each do |element|
+      array << Profile.find(element).technical.followers # ajouter celui du current user
+    end
+    max_score = array.max
+    (1-(difference/max_score)) * 100
   end
 
   def experience_score
-  # Objectif: definir un score_experience en fonction de 4 KPI, en utilisant la méthode GoodBuddy
-      # experience_score = (github_age_score + number_of_projects_score + total_commits_score + followers_score) / 4
+    experience_score = (github_age_score + number_of_projects_score + total_commits_score + followers_score) / 4
   end
-
-  def github_age_score
-    # profiles.commun_goal.all.github_age.each do |profile|
-    #   score_1 = ((current_profile.technicals.github_age - profile.technicals.github_age).abs)
-    # end
-    # profiles.commun_goal.all.github_age.each do |profile|
-    # score_2 = (1 - (score_1/score_1_array.max)) x 100
-
-    # end
-  end
-
 
 #________________________________________________________________________________________________________________________________
 
