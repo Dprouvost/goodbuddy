810cf89a5849111b9291bb9641e60f4dce564638
Rodolphe Desobry <rodolphe.desobry@grenoble-em.com>
2019-03-08 17:03:24 +0100

    WIP

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -136,7 +136,7 @@ class ProfilesController < ApplicationController
   end
 
   def create
-    # @profile = Profile.new(params_profile)
+    # @profile = Profile.new(profile_settings)
     # if @profile.save
     #   redirect_to root_path #mettre la route de la roue
     # else
@@ -145,6 +145,7 @@ class ProfilesController < ApplicationController
   end
 
   def edit
+    
   end
 
   def show
@@ -157,7 +158,7 @@ class ProfilesController < ApplicationController
   private
 
     def profile_params
-      # params.require(@:rofile).permit(:nickname, :picture, :location, :description)
+      # params.require(@profile).permit(:nickname, :picture, :location, :description)
     end
 
     def set_profile
