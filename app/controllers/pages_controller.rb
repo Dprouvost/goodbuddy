4b16314eb2af6473ff48a9ef8dd4ad0e0a762cd4
theo watine <watinetheo@gmail.com>
2019-03-14 11:20:24 +0100

    social page is finally fucking done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,6 +20,10 @@ class PagesController < ApplicationController
     @main_categories = Category.where(category_id: nil, stamp: 'social')
     # @categories = Category.where(stamp: 'goal')
     # @profile = Profile.find(3)
+
+    @pic_titles = ["Would you rather chill out with friends or go to gigs?", "Would you rather go to the cinema or watch netflix?", "Would you rather eat a burger or a salad?", "Are you an organized or a disorganized type of person?"]
+    @first_options = ["1", "2", "3", "4"]
+    # @second_options =
   end
 
   def select_goals
